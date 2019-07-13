dbf4f92d51da519c11cfeb9e32fc46fa36fc9fa3
John Merchant <john@merchant.on.net>
2019-07-13 12:50:35 +1000

    Test single day

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const intervalSplit = require('.');
 it('does not split an interval on the same day', () => {
     const start = new Date('2019-06-01T12:00:00');
     const end = new Date('2019-06-01T15:00:00');
-    const result = intervalSplit(start, end, '08:30:00', '05:00:00');
+    const result = intervalSplit(start, end, '08:30:00', '17:00:00');
     expect(result).toEqual([{ start, end }]);
 });
 
