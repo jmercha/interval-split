069fdddac3b8f9dbff8a195e727b118628f8a74e
John Merchant <john@merchant.on.net>
2019-07-13 12:28:57 +1000

    remove unused vars

--- index.test.js
+++ index.test.js
@@ -2,12 +2,7 @@ const moment = require('moment');
 const intervalSplit = require('.');
 
 it('splits an interval', () => {
-
-    const startDate = moment('2019-06-01T12:00:00');
-    const endDate =  moment('2019-06-03T12:00:00');
-    const startTime = moment('08:30:00', 'hh:mm:ss');
-    const endTime = moment('05:00:00', 'hh:mm:ss');
-
+    
     const result = intervalSplit(
         new Date('2019-06-01T12:00:00'),
         new Date('2019-06-03T12:00:00'),
